d901b404a9b0947b63d8d937fec149bd8c8115f6
NomadicHippiee <nomadichippie@proton.me>
2026-07-15 23:24:18 +0200

    Add service controller with list detail endpoints

---
+++ controllers/serviceController.js
@@ -0,0 +1,30 @@
+const pool = require('../db/db');
+
+exports.getAllServices = async (req, res) => {
+    try {
+        const result = await pool.query('SELECT * FROM services');
+        const services = result.rows;
+        res.render('service_list', { services });
+    } catch (error) {
+        console.error('Error fetching services: ', error);
+        res.status(500).send('Error fetching services');
+    }
+};
+
+exports.getServiceDetail = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const result = await pool.query('SELECT * FROM services WHERE id = $1', [id]);
+
+        if (result.rows.length === 0) {
+            return res.status(404).send('Service not found');
+        }
+        
+        const service = result.rows[0];
+        res.render('service_detail', { service });
+    } catch (error) {
+        console.error('Error fetching service', error);
+        res.status(500).send('Error fetching service')
+        
+    }
+}
