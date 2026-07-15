e909577911e7848c852925bbcfb301079da3b135
NomadicHippiee <nomadichippie@proton.me>
2026-07-15 23:25:49 +0200

    Connect to indexRouter

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
+app.use('/', require('./routes/indexRouter'));
 
 app.use((err, req, res, next) =>{
     console.error(err);
