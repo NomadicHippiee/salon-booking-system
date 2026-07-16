8e72a59d5a7fa0ab08c70fec0df379152c409336
NomadicHippiee <nomadichippie@proton.me>
2026-07-16 19:56:16 +0200

    install and setup EJS layouts

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const path = require('path');
+const expressEjsLayouts = require('express-ejs-layouts');
 require('dotenv').config();
 
 const app = express();
@@ -12,6 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+
+app.use(expressEjsLayouts);
 
 app.use('/', require('./routes/indexRouter'));
 
