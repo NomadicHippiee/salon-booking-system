b3d40b89eb4dda8604264800654604a04a3349f7
NomadicHippiee <nomadichippie@proton.me>
2026-07-16 14:00:00 +0200

    Update indexRouter with new routes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const router = express.Router(); 
 const serviceController = require('../controllers/serviceController');
+const bookingController = require('../controllers/bookingController');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
 
 router.get('/services', serviceController.getAllServices);
+router.get('/services/new', serviceController.getNewServiceForm);
+router.post('/services', serviceController.createService);
 router.get('/services/:id', serviceController.getServiceDetail);
+router.get('/services/:id/edit', serviceController.getEditServiceForm);
+router.post('/services/:id', serviceController.updateService);
+
+router.get('/bookings', bookingController.getAllBookings);
+router.get('/bookings/new', bookingController.getNewBookingForm);
+router.post('/bookings', bookingController.createBooking);
 
 module.exports = router;
