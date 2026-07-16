076614139cf51f0c10192b887ca442ca0d6dde18
NomadicHippiee <nomadichippie@proton.me>
2026-07-16 20:42:44 +0200

    Add delete routes to indexRouter

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -13,9 +13,13 @@ router.post('/services', serviceController.createService);
 router.get('/services/:id', serviceController.getServiceDetail);
 router.get('/services/:id/edit', serviceController.getEditServiceForm);
 router.post('/services/:id', serviceController.updateService);
+router.get('/services/:id/delete', serviceController.getDeleteServiceForm);
+router.post('/services/:id/delete', serviceController.deleteService);
 
 router.get('/bookings', bookingController.getAllBookings);
 router.get('/bookings/new', bookingController.getNewBookingForm);
 router.post('/bookings', bookingController.createBooking);
+router.get('/bookings/:id/delete', bookingController.getDeleteBookingForm);
+router.get('/bookings/:id/delete', bookingController.deleteBooking);
 
 module.exports = router;
